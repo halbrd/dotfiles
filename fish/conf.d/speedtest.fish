b48250899e1a2a0f4ec5fcd0a7267b42f72708fd
William Holt <halbrd@gmail.com>
2019-06-06 23:23:57 +1000

    develop fish speedtests

--- fish/conf.d/speedtest.fish
+++ fish/conf.d/speedtest.fish
@@ -1,1 +1,18 @@
-abbr --add speedtest "curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -"
+# internet speed test
+function netspeed
+  curl -s https://gist.githubusercontent.com/halbrd/bafbeab36770134dc181237792af458e/raw/7f2aab43b040cc2a90f9106500d85a3e2b9fc540/speedtest.py | python -
+end
+
+# disk speed tests
+function diskspeed
+  for path in $argv
+    if test -e "$path/diskspeed"
+      echo "ERROR: $path/diskspeed already exists"
+    else
+      sudo /sbin/sysctl -w vm.drop_caches=3 >/dev/null
+      echo $path "write:" (sync; and dd if=/dev/zero of="$path/diskspeed" bs=1M count=1024 2>&1 >/dev/null; and sync) | cut -d " " -f 1,2,11-
+      echo $path "read:" (sync; and dd if="$path/diskspeed" of=/dev/null bs=1M count=1024 2>&1 >/dev/null; and sync) | cut -d " " -f 1,2,11-
+      rm "$path/diskspeed"
+    end
+  end
+end
