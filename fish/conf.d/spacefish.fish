6e7a32bfaf239982e97c929e4dfb3baee3342443
William Holt <william.holt@digio.com.au>
2019-07-31 21:20:50 +1000

    add spacefish config

---
+++ fish/conf.d/spacefish.fish
@@ -0,0 +1,44 @@
+# general
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+set SPACEFISH_PROMPT_ORDER user host dir git venv node package golang jobs line_sep char
+set SPACEFISH_RPROMPT_ORDER time
+
+# user
+set SPACEFISH_USER_SHOW always
+set SPACEFISH_USER_COLOR blue
+
+# host
+set SPACEFISH_HOST_SHOW always
+set SPACEFISH_HOST_PREFIX 'on '
+set SPACEFISH_HOST_COLOR red
+
+# dir
+set SPACEFISH_DIR_TRUNC 0
+set SPACEFISH_DIR_TRUNC_REPO false
+set SPACEFISH_DIR_COLOR blue
+
+# git
+set SPACEFISH_GIT_PREFIX 'working on '
+
+# venv
+set SPACEFISH_VENV_PREFIX 'venv '
+set SPACEFISH_VENV_SYMBOL ''
+
+# node
+set SPACEFISH_NODE_PREFIX 'node '
+set SPACEFISH_NODE_SYMBOL ''
+
+# package
+set SPACEFISH_PACKAGE_PREFIX ''
+
+# golang
+set SPACEFISH_GOLANG_PREFIX 'go '
+set SPACEFISH_GOLANG_SYMBOL ''
+
+# jobs
+
+# char
+set SPACEFISH_CHAR_SYMBOL '$'
+set SPACEFISH_CHAR_COLOR_SUCCESS white
+set SPACEFISH_CHAR_COLOR_FAILURE red
+
