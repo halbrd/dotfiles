feb481abb4e98f4d12e07453c52ddd2bfa8d2269
William Holt <halbrd@gmail.com>
2019-10-02 22:30:33 +1000

    fix music-wrangling.fish output file

--- fish/conf.d/music-wrangling.fish
+++ fish/conf.d/music-wrangling.fish
@@ -8,8 +8,10 @@ function ffpg --argument src_fmt dst_fmt src_path
 
     echo "[$dst_fmt] <= $file"
 
+	set dst_file (echo (basename $file) | sed -e 's/.flac$/.m4a/g')
+
     if [ $dst_fmt = 'm4a' ]
-      ffmpeg -y -loglevel warning -i $file -map 0:a -c:a aac -b:a 257k (dirname $file)"_$dst_fmt/"(basename $file); or return
+      ffmpeg -y -loglevel warning -i $file -map 0:a -c:a aac -b:a 257k (dirname $file)"_$dst_fmt/$dst_file"; or return
     else if [ $dst_fmt = 'mp3' ]
       ffmpeg -y -loglevel warning -i $file -q:a 0 -map_metadata 0 -id3v2_version 4 (dirname $file)"_$dst_fmt/"(basename $file); or return
     else
