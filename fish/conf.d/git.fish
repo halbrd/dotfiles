045297e84f2cdd4f2525b0f1a2053ebd70b518a8
William Holt <halbrd@gmail.com>
2020-06-24 17:05:46 +1000

    improve git abbreviations

--- fish/conf.d/git.fish
+++ fish/conf.d/git.fish
@@ -1,13 +1,14 @@
-abbr --add gst "git status"
-abbr --add gcb "git checkout -b"
-abbr --add ga "git add"
-abbr --add gaa "git add --all"
-abbr --add gdca "git diff --cached"
-abbr --add gcm "git commit -m"
-abbr --add ggpush "git push origin HEAD"
-abbr --add ggpull "git pull origin HEAD"
-abbr --add gdl "git diff HEAD~ HEAD"
-abbr --add gbd "git branch -d"
-abbr --add gcam "git commit --amend"
-abbr --add gcam! "git commit --amend --no-edit"
-
+abbr --add gst 'git status'
+abbr --add gcb 'git checkout -b'
+abbr --add ga 'git add'
+abbr --add gaa 'git add --all'
+abbr --add gd 'git diff'
+abbr --add gdca 'git diff --cached'
+abbr --add gcm 'git commit -m \'\''
+abbr --add ggpush 'git push origin (git rev-parse --abbrev-ref HEAD)'
+abbr --add ggpull 'git pull origin (git rev-parse --abbrev-ref HEAD)'
+abbr --add gdl 'git diff HEAD~ HEAD'
+abbr --add gbd 'git branch -d'
+abbr --add gcam 'git commit --amend'
+abbr --add gcam! 'git commit --amend --no-edit'
+# git log abbreviation intentionally excluded until I have a favourite invokation
