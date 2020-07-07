01da364e338d8394d29dd0072212f12010d13188
William Holt <halbrd@gmail.com>
2020-07-07 17:09:33 +1000

    fix syntax of lifx-toggle function

--- fish/conf.d/lifx.fish
+++ fish/conf.d/lifx.fish
@@ -12,6 +12,6 @@ end
 
 function lifx-toggle
   set current_state (lifx lan:get_attr $argv power --silent | sed -E 's/\w+: \{\"level\": (\w+)}/\1/g')
-  set new_state (test 0 -eq $current_state && echo on || echo off)
+  set new_state (test 0 -eq $current_state; and echo on; or echo off)
   lifx lan:transform $argv --silent -- "{\"power\":\"$new_state\"}"
 end
