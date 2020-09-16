61c7c1585a45fb51fb3556da42c6f9d1543c8668
William Holt <halbrd@gmail.com>
2020-09-17 09:53:15 +1000

    improve warming functions in fish

--- fish/conf.d/warm.fish
+++ fish/conf.d/warm.fish
@@ -1,4 +1,4 @@
-function warm
+function warmer
   set -q argv[1]; or set argv[1] 4
   for i in (seq 1 $argv[1])
     yes > /dev/null &
@@ -7,4 +7,8 @@ end
 
 function unwarm
   killall yes
+end
+
+function warms
+  ps Taux | grep yes | grep -v grep
 end
