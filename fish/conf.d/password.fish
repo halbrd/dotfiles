f41e147da15b7c3db70e36bc291d3c65bb50c314
William Holt <halbrd@gmail.com>
2021-05-12 14:26:26 +1000

    add left/right handed password commands

---
+++ fish/conf.d/password.fish
@@ -0,0 +1,2 @@
+abbr --add leftypw "xkcdpass -n 500 -d \n | uniq | sort | grep -E '^[qwertyasdfghzxcvb]+\$'"
+abbr --add rightypw "xkcdpass -n 500 -d \n | uniq | sort | grep -E '^[tyuiopghjklbnm]+\$'"
