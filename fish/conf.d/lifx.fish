0431db7d147ca4e27329cf5e3d148a00ec741889
William Holt <halbrd@gmail.com>
2021-05-14 18:39:05 +1000

    update lifx-toggle to work with new lifx-photons api

--- fish/conf.d/lifx.fish
+++ fish/conf.d/lifx.fish
@@ -11,7 +11,7 @@ function lifx-on
 end
 
 function lifx-toggle
-  set current_state (lifx lan:get_attr $argv power --silent | sed -E 's/\w+: \{\"level\": (\w+)}/\1/g')
+  set current_state (lifx lan:attr $argv GetPower --silent | sed -E 's/\w+: \{\"level\": (\w+)}/\1/g')
   set new_state (test 0 -eq $current_state; and echo on; or echo off)
   lifx lan:transform $argv --silent -- "{\"power\":\"$new_state\"}"
 end
